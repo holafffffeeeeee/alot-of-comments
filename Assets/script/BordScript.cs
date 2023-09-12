5c05e6cfe8ce45023f6b25fbcc95d426a7f29c56
holafffffeeeeee <lukas.gerestrand@gmail.com>
2023-09-12 09:01:21 +0200

    Initial commit

---
+++ Assets/script/BordScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BordScript : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float id;
+    public float moveSpeed = 1.0f;
+
+    public Transform ballPos;
+
+    private void Update()
+    {
+        float movement = ProcessInput();
+        Move(movement);
+    }
+
+    private float ProcessInput()
+    {
+        float movement = 0f;
+        switch (id)
+        {
+            case 1:
+                movement = Input.GetAxis("movePlayer1");
+                break;
+
+            case 2:
+                movement = Input.GetAxis("movePlayer2");
+                break;
+
+            case 3:
+
+                //Debug.Log("normed   "+ballPos.position.normalized.y);
+
+                movement = ballPos.position.normalized.y;
+
+                break;
+
+                
+
+        }
+        return movement;
+    }
+  /*  public void If() 
+    {
+        if (dir <= moveSpeed) ;
+    }
+       */
+   private void Move(float movement)
+    {
+        Vector2 valo = rb2d.velocity;
+        valo.y = moveSpeed * movement;
+        rb2d.velocity = valo;
+    }
+}
